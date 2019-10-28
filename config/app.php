77a57d2a9b7e3e78d262ee0ebd144864d6733766
pablonataniel76 <pablo.nataniel76@gmail.com>
2019-10-28 00:13:55 -0400

    config/app arregladoV2.0

--- config/app.php
+++ config/app.php
@@ -228,8 +228,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         // ...
-        'Form' => CollectiveHtmlFormFacade::class,
-        'Html' => CollectiveHtmlHtmlFacade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
         // ...
 
     ],
